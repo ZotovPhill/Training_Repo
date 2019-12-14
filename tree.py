d18c51999c3e4baef271e5d22d4f753ab1850bd7
ZotovPhill <phill.zotov@gmail.com>
2019-12-15 01:26:37 +0300

    Delete tree.py

--- tree.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Введите количесво уровней елки: \n"))
-branch = 1
-while n != 0:
-    layer = f"{' '*(n-1)}{'^'*branch}"
-    print(layer)
-    n -= 1
-    branch += 2
-bottom = f"{' '*(branch//2 -2)}| |"
-print(bottom)
