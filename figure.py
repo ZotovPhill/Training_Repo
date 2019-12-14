31e97c56b7d81d01741203824e35507f216a0872
Phill Zotov <phill.zotov@gmail.com>
2019-12-15 01:58:15 +0300

    Some changes in figure

--- figure.py
+++ figure.py
@@ -9,15 +9,15 @@ space = int(input("Введите длину строки: "))
 rows = int(input("Введите количество строк: "))
 new_row, new_space = rows, space
 stick = 1
+middle_space = new_row // 2 + 1
 while rows != 0:
-    if rows > new_row // 2 + 1:
+    if rows > middle_space:
         print(draw_line(space, stick))
         space -= 6
         stick += 2
-    elif rows == new_row // 2 + 1:
+    elif rows == middle_space:
         print(draw_line(new_space, welcome=True))
     else:
-        welcome = False
         space += 6
         stick -= 2
         print(draw_line(space, stick))
